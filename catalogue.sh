2e333a74c694e01ed7eed1ccbe7eb837fcdc224f
tomalarajesh <tomalaraj@gmail.com>
2026-02-16 19:28:20 +0530

    hello world

--- catalogue.sh
+++ catalogue.sh
@@ -71,3 +71,4 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 mongosh --host rajdevops.fun </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Loading Data into MongoDB"
