3822a9fe83bcb32faf0f08c6a795ad700c1f374a
tomalarajesh <tomalaraj@gmail.com>
2026-02-16 23:00:30 +0530

    hello world

--- mongodb.sh
+++ mongodb.sh
@@ -44,7 +44,7 @@ VALIDATE $? "Enabling MongoDB"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
