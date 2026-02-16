1005d0948144ea69bd058a5b1a8b2711b0717b67
tomalarajesh <tomalaraj@gmail.com>
2026-02-16 18:53:18 +0530

    hello world

--- mongodb.sh
+++ mongodb.sh
@@ -12,39 +12,39 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# checking the user has root priveliges or not 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
-then 
+then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 # give  other than 0 to 127 
-else 
+    exit 1 #give other than 0 upto 127
+else
     echo "You are running with root access" | tee -a $LOG_FILE
-fi 
+fi
 
-# validate functtions takes input as exit status, what command they tried to install 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1 
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongodb.repo"
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb server"
 
-systemctl enable mongodb &>>$LOG_FILE
-VALIDATE $? "Enabiling MongoDB"
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "starting MongoDB"
+VALIDATE $? "Starting MongoDB"
 
-sed -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
